f1456ad533944138e7bd4d96ecce3944c3533dae
19jy0232 <19jy0232@PC06.jynet2.jec.ac.jp>
2020-11-12 10:03:03 +0900

    テスト

--- Java project/src/project/Helloworld.java
+++ Java project/src/project/Helloworld.java
@@ -1,10 +1,15 @@
 package project;
 
 public class Helloworld {
+<<<<<<< test
 	public static void main(String[] args) {
 		for(int i=0;i<=args.length;i++) {
 		System.out.println("Hello World"+args[i]);
 		}
 
+=======
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+>>>>>>> e19895a Hello Worldプラスユーザー名の表示
 	}
 }
